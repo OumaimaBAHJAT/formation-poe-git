22b6e7cb4717bdc813ea4a9f687ce19e6a82d7a2
Oumaima BAHJAT <oumaima.bahjat11@gmail.com>
2022-01-17 15:17:59 +0100

    validator class

---
+++ js/validator.js
@@ -0,0 +1,18 @@
+class Validator {
+
+    isRequired(str) {
+    
+    return value.length > 0 || value !== undefined;
+    
+    }
+    
+    }
+    class Validator {
+
+        isRequired(str) {
+        
+        return str.length > 0 || str !== undefined;
+        
+        }
+        
+        }
